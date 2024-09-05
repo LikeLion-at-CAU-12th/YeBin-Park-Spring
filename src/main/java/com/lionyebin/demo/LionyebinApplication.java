97f089e6cba50274d6f84d86a0caf25908c35cf4
yebinnnnn <prettybird1@naver.com>
2024-09-05 20:16:32 +0900

    [INIT]깃 연동

---
+++ src/main/java/com/lionyebin/demo/LionyebinApplication.java
@@ -0,0 +1,13 @@
+package com.lionyebin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionyebinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LionyebinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lionyebin/demo/LionyebinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionyebin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionyebinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
