561b9d8c2f4ce49effe51f5bfc43bc246b3caa0e
yebinnnnn <prettybird1@naver.com>
2024-09-24 18:30:09 +0900

    [INIT]세션실습파일

---
+++ src/main/java/com/lionyebin/demo/domain/Comment.java
@@ -0,0 +1,16 @@
+package com.lionyebin.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+public class Comment {
+    @Id @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+    private String content;
+    @ManyToOne
+    @JoinColumn(name = "article_id")
+    private Article article;
+}
--- src/main/java/com/lionyebin/demo/domain/Member.java
+++ src/main/java/com/lionyebin/demo/domain/Member.java
@@ -17,21 +17,9 @@ public class Member extends BaseTimeEntity{
     @Column(name="member_id")
     private long id;
 
-    @Column(nullable = true)
+    @Column(nullable = false)
     private String username;
     private String email;
     private int age;
-
-    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL) //한멤버는 여러개의 플리생성
-    private List<PlayList> playlists = new ArrayList<>();
-    public void addPlaylist(PlayList playlist){ // 플레이리스트 생성함수
-        this.playlists.add(playlist);
-        playlist.setMember(this); //이게 없으면 PlayList 의 Member 가 null이 되버릴수도 있단겨
-    }
-
-    public void removePlaylist(PlayList playList){ //플레이리스트 삭제함수
-        this.playlists.remove(playList);
-        playList.setMember(null);
-    }
 
 }
